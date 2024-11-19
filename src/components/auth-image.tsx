bc1058b4311b7997d9a2dc3fc93a243f2cf299d1
Anas Chebili <anaschebili@gmail.com>
2024-11-19 18:29:53 +0100

    improved auth-image component

--- src/components/auth-image.tsx
+++ src/components/auth-image.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function AuthImage() {
   return (
-    <div className="fixed  w-1/2 h-full overflow-hidden">
+    <section className="fixed  w-1/2 h-full overflow-hidden">
       <div className="h-full  hidden sm:block md:h-auto w-full">
         <Image
           src="authart.svg"
@@ -19,23 +19,29 @@ export default function AuthImage() {
         width={72}
         className="absolute top-12 left-12"
       ></Image>
-      <div className=" absolute top-72 -right-44 rotate-90 hidden md:block">
+      <section className=" absolute top-72 -right-44 rotate-90 hidden md:block">
         <h1 className="text-3xl font-semibold tracking-wider">
           CHANGE THE WORLD. NEVER SETTLE
         </h1>
-        <div className="flex">
+        <section className="flex">
           <h1 className="text-3xl font-semibold tracking-wider">NEVER FEAR.</h1>
           <div className=" ml-4 mt-[5px] h-6 w-[343px] rounded-3xl border-[3px] border-white "></div>
-        </div>
-        <div className="flex items-center space-x-[260px]">
+        </section>
+        <section className="flex items-center space-x-[260px]">
           <p className="text-xs font-semibold">3431-24190-21ASX-AZCV4-DGTPS1</p>
-          <div className="flex space-x-2">
-            <Image src="/star.png" alt="" height={32} width={30}></Image>
-            <Image src="/star.png" alt="" height={32} width={30}></Image>
-            <Image src="/star.png" alt="" height={32} width={30}></Image>
-          </div>
-        </div>
-      </div>
-    </div>
+          <ul className="flex space-x-2">
+            <li>
+              <Image src="/star.png" alt="" height={32} width={30}></Image>
+            </li>
+            <li>
+              <Image src="/star.png" alt="" height={32} width={30}></Image>
+            </li>
+            <li>
+              <Image src="/star.png" alt="" height={32} width={30}></Image>
+            </li>
+          </ul>
+        </section>
+      </section>
+    </section>
   );
 }
