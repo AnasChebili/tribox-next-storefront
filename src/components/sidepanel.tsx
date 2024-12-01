5ca93fed56ec11613b027938e1a6565331408cdb
Anas Chebili <anaschebili@gmail.com>
2024-12-01 16:59:28 +0100

    improved sematical structure of side panel component

--- src/components/sidepanel.tsx
+++ src/components/sidepanel.tsx
@@ -2,12 +2,12 @@ import { SidebarMenuItem } from "./sidebar-menu-item";
 
 export default function SidePanel() {
   return (
-    <div className=" hidden xl:block xl:w-[300px] p-6 h-[calc(100%-146px)] bg-gray-800   text-gray-50 rounded-xl lg:fixed">
-      <div>
+    <section className=" hidden xl:block xl:w-[300px] p-6 h-[calc(100%-146px)] bg-gray-800   text-gray-50 rounded-xl lg:fixed">
+      <section>
         <SidebarMenuItem href="/dashboard">Dashboard</SidebarMenuItem>
         <SidebarMenuItem href="/dashboard/profile">Profile</SidebarMenuItem>
         <SidebarMenuItem href="/dashboard/settings">Settings</SidebarMenuItem>
-      </div>
-    </div>
+      </section>
+    </section>
   );
 }
